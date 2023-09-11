2faf9d9de39e2b6e2c7bc24ac701d54e0d0e3094
envvoo <eoni090@gmail.com>
2023-09-11 23:58:47 +0900

    Refactor: 중복 코드 제거 및 파일 병합

--- JS/standard-time.js
+++ JS/standard-time.js
@@ -1,14 +1,30 @@
 const standard = document.querySelector(".chart__standardTime");
+const textArr = standard.innerHTML.split(":");
+let [standardYear, standardMonth, standardDate, standardHour] = [
+  textArr[0],
+  textArr[1],
+  textArr[2],
+  textArr[3],
+];
 
 function getTime() {
   const standardTime = new Date();
-  const standardYear = standardTime.getFullYear();
+  standardYear = standardTime.getFullYear();
   const strMonth = String(standardTime.getMonth() + 1);
-  const standardMonth = strMonth.length == 1 ? 0 + strMonth : strMonth;
+  standardMonth = strMonth.length == 1 ? 0 + strMonth : strMonth;
   const strDate = String(standardTime.getDate());
-  const standardDate = strDate.length == 1 ? 0 + strDate : strDate;
+  standardDate = strDate.length == 1 ? 0 + strDate : strDate;
 
-  standard.innerHTML = `${standardYear}.${standardMonth}.${standardDate} 기준`;
+  standard.innerHTML = `${standardYear}.${standardMonth}.${standardDate}`;
+
+  if (standardHour !== undefined) {
+    standardHour = standardTime.getHours();
+    const emphasis = document.createElement("span");
+    emphasis.style.fontWeight = "600";
+    emphasis.innerHTML = ` ${standardHour}:00 `;
+    standard.appendChild(emphasis);
+    standard.append("기준");
+  } else standard.append(" 기준");
 }
 
 getTime();
